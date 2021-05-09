0758a513e643f5ddfccf90ac62c7af728fff0829
aRTist <ruantian@seu.edu.cn>
2021-05-09 17:31:49 +0800

    date

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,7 +23,17 @@ def create_app(test_config=None):
 
     @app.route('/hello')
     def hello():
-        return "Hello World!"
+        return "Hello, World!"
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
